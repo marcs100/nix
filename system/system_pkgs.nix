6792689893b8a917a76e718982ae8f1e083d74bb
marcs100 <marcs100@gmail.com>
2024-09-04 20:34:49 +0100

    added a file

---
+++ system/system_pkgs.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+
+{
+ environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+	vim
+	brave
+	git
+	curl
+	wget
+	gnome.gnome-software
+	gnome-extension-manager
+	gnome.gnome-tweaks
+	syncthing
+	floorp
+  ];
+{
