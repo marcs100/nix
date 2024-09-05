a309a2564f457296af0c6f7174d1b8558b7d57d4
marcs100 <marcs100@gmail.com>
2024-09-05 21:47:24 +0100

    Update plasma.nix

--- system/plasma.nix
+++ system/plasma.nix
@@ -1,4 +1,32 @@
 { config, pkgs, ... }:
 {
-
-}
+    
+    # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+  
+    environment.pkgs.excludePackages = (with pkgs; [
+      # for packages that are pkgs.*
+      gnome-tour
+      gnome-music
+      epiphany
+      simple-scan
+    ]) ++ (with pkgs.kdepackages; [
+      # for packages that are pkgs.gnome.*
+      #evince # document viewer
+    ]);
+  
+   environment.systemPackages = with pkgs; [
+	  gnome-software
+	gnome-extension-manager
+	gnome-tweaks
+	gnome-boxes
+	gnome-bluetooth
+	# extensions ----------
+	gnomeExtensions.tray-icons-reloaded
+	gnomeExtensions.dash-to-dock
+	#gnomeExtensions.no-overview
+	gnomeExtensions.resource-monitor
+	#----------------------
+  ];
+};
