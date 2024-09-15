2f75220765e721f2ab3ced0b3d54c77d25252d0e
marcs100 <marcs100@gmail.com>
2024-09-15 10:52:37 +0100

    Remove virtulisation from here

--- system/system_pkgs.nix
+++ system/system_pkgs.nix
@@ -1,14 +1,5 @@
 { config, pkgs, ... }:
 {
-
-    virtualisation.containers.enable = true; 
-    virtualisation.podman = {
-         enable = true;
-        dockerCompat = true;
-
-    };
-
-
     environment.systemPackages = with pkgs; [
         vim #this will be managed by home manager
         brave
