ede19df0b0a1782525992cc18fcecbc03a37566a
marcs100 <marcs100@gmail.com>
2024-09-30 09:03:52 +0100

    Added server hostname (vault-tec) mapped to 192.168.1.200

--- system/settings.nix
+++ system/settings.nix
@@ -55,6 +55,11 @@
     networking.hostName = "vortex"; # Define your hostname.
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+    networking.extraHosts =
+    ''
+        192.168.1.200 vault-tec
+    '';
+
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
